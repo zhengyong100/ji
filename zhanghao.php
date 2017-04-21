dc2da9fe1cc74ac466ea76020fa0f3d4610d7794
zheng yong <25812739@qq.com>
2017-04-21 15:56:59 -0300

    Update zhanghao.php

--- zhanghao.php
+++ zhanghao.php
@@ -27,8 +27,8 @@ $row = mysql_fetch_array($result2);
 //-->
 </script>
 		 
-        用户名：<?echo $row[username]; ?> <a href="javascript:linkok('shanchu.php?uid=$_SESSION[uid]')">清除全部数据</a>（操作前请备份导出）<br /><br />
-注册时间：<?echo date("Y-m-d H:i",$row[utime]); ?><br /><br />
+        用户名：<?echo $row[username]; ?> <!-- <a href="javascript:linkok('shanchu.php?uid=$_SESSION[uid]')">清除全部数据</a>（操作前请备份导出）--><br /><br />
+		注册时间：<?echo date("Y-m-d H:i",$row[utime]); ?><br /><br />
         电子邮箱 <input for="Email" name="email" id="email" value="<?echo $row[email]; ?>"> <br /><br />
         旧密码：<input type="password" name="jiupassword" id="jiupassword" size="18" maxlength="15" /><br /><br />
         新密码：<input type="password" name="password" id="password" size="18" maxlength="15" /> *(最多15个字符) <br /><br />
@@ -60,11 +60,38 @@ $row = mysql_fetch_array($result2);
 
    
     </form>
-          
+	
+  
           </td>
         </tr>
       </table>
 
+<table align="center" width="100%" border="0" cellpadding="5" cellspacing="1" bgcolor='#B3B3B3' class='table table-striped table-bordered'>
+<tr>
+          <td bgcolor="#EBEBEB">　清空全部数据</td>
+        </tr>
+        <tr>
+          <td bgcolor="#FFFFFF">
+		  密码：<input type="password" name="mimayanzheng" id="mimayanzheng" size="18" maxlength="15" /> （操作前请备份导出）<br /><br />
+		  <input name="shangchushuju" type="submit" value="清除全部数据" class="btn btn-default" />
+		<?php 
+        if($_POST[shangchushuju]){  
+         $mmyanzheng=md5($_POST[mimayanzheng]);
+			if($mmyanzheng==$row[password]){
+				echo "<meta http-equiv=refresh content='2; url=shanchu.php?uid=".$_SESSION[uid].")'>";
+				echo "<font color='green'>已全部删除成功！</font>";
+				}else{
+					echo "<font color='red'>密码错误！</font>";
+					}
+		}
+?>
+		<br /><br />
+		  
+          </td>
+        </tr>		  
+     </table>   
+	  
+	  
 <?php
     include_once("xiamian.php");
 ?>
